202be0c54c0e8ec6ab03ea331a64c9e48dfb583f
Vignesh Nanduri <vignesh.nanduri@gmail.com>
2024-10-20 21:05:03 -0400

    Vignesh Nanduri Lab 6 encode portion

---
+++ main_lab_6.py
@@ -0,0 +1,42 @@
+#Vignesh Nanduri
+
+def encode(password):
+    encoded_password = ''
+    for i in range(len(password)):
+        encoded_password += str(3 + int(password[i]))
+    return encoded_password
+
+def decode(encoded_password):
+    pass
+
+def main():
+    print('Menu')
+    print('-------------')
+    print('1. Encode')
+    print('2. Decode')
+    print('3. Quit')
+    user_option = int(input('\nPlease enter an option: '))
+    prompt = True
+    while prompt:
+        if user_option == 1:
+            password = input('Please enter your password to encode: ')
+            print('Your password has been encoded and stored!')
+            continue_menu = True
+        elif user_option == 2:
+            pass
+        elif user_option == 3:
+            prompt = False
+            continue_menu = False
+        while continue_menu:
+            if user_option == 1:
+                password = input('Please enter your password to encode: ')
+                print('Your password has been encoded and stored!')
+                continue_menu = True
+            elif user_option == 2:
+                pass
+            elif user_option == 3:
+                prompt = False
+                continue_menu = False
+
+if __name__ == '__main__':
+    main()
